c000a88a251c2fdc34218d2aa37e6e39cd44ab38
AzisMM <mabdulaziz124@gmail.com>
2021-03-18 09:43:28 +0700

    Initial commit

---
+++ src/com/azis/javafundamental/CallFunction.java
@@ -0,0 +1,15 @@
+package com.azis.javafundamental;
+
+public class CallFunction {
+    public static void main(String[] args) {
+        //memanggil function
+
+        cobaFungsi();
+    }
+
+    private static void cobaFungsi() {
+        System.out.println("Ini merupakan bagian dari fungsi");
+    }
+
+
+}
